121b11666ad0787c0a17a10c39e37069033be85a
Kedrorex <belousovvk@bk.ru>
2023-03-13 16:28:23 +0300

    inicilizacia

---
+++ HJ2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int RoadTon(int N, int M)
+{
+
+    if (N == M)
+    {
+        return M;
+    }
+    return M + RoadTon(N, M + 1);
+
+}
+
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(RoadTon(numberB, numberA));
